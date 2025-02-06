11aeca22ddf54255762880ffcf010323b47f812f
matusz019 <65133130+matusz019@users.noreply.github.com>
2025-02-06 16:14:15 +0000

    Added fragments and styling to bottom nav bar

--- app/src/main/java/com/example/moneyglitch_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/moneyglitch_kotlin/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener {menuItem ->
+            when(menuItem.itemId){
+                R.id.bottom_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.bottom_budget -> {
+                    replaceFragment(BudgetFragment())
+                    true
+                }
+                R.id.bottom_trends -> {
+                    replaceFragment(TrendsFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+        replaceFragment(HomeFragment())
+        bottomNavigationView.selectedItemId = R.id.bottom_home
+    }
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
     }
 }
