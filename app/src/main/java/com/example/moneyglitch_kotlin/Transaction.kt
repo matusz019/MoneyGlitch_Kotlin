a81566b4db87ca4c5d0b83c93463089ab4e7dd73
matusz019 <65133130+matusz019@users.noreply.github.com>
2025-02-19 15:19:10 +0000

    Added Transaction.kt

--- app/src/main/java/com/example/moneyglitch_kotlin/Transaction.kt
+++ app/src/main/java/com/example/moneyglitch_kotlin/Transaction.kt
@@ -1,5 +1,15 @@
 package com.example.moneyglitch_kotlin
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "transactions")
 data class Transaction(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val amount: Double,
+    val date: String,
+    val description: String,
+    val category: String,
+    val type: String  // "income" or "expense"
 )
